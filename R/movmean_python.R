84c14e518172ab63409e0c3e8ed64020a33814f1
Qitao Cui <1625713288@qq.com>
2020-11-30 02:51:05 +0800

    Revert "崔骐韬20181001697"
    This reverts commit 22e86e560a87adab72afbf846b565c4f24b302fb.

--- R/movmean_python.R
+++ R/movmean_python.R
@@ -1,5 +1,29 @@
-#' @export 
+#' @export
 movmean_python <- function(x, halfwin) {
+  import numpy as np
+  def movemean(x,halfwin):
+    n=len(x)
+  if halfwin>n/2:
+    return print("错误！！！半窗长度过长！！！")
+
+  y=np.zeros(n)
+  if halfwin==1:
+    y[0]=(x[0]+x[1])/2
+  for i in range(1,n-1):
+    y[i]=(x[i]+x[i-1]+x[i+1])/3
+  y[n-1]=(x[n-2]+x[n-1])/2
+  else:
+    m=halfwin
+  for i in range(0,n):
+    if i<m:
+    inds=np.arange(0,(i+m)+1)
+  y[i]=(np.sum(x[inds]))/len(inds)
+  elif i>=m and i<=n-m-1:
+    y[i]=(np.sum(x[i-m:i+m+1]))/(2*m+1)
+  else:
+    inds=np.arange(i-m,n)
+  y[i]=(np.sum(x[inds]))/len(inds)
+  return y
 
 }
 
@@ -7,7 +31,7 @@ movmean_python <- function(x, halfwin) {
 #' @importFrom reticulate source_python
 init_python < - function(){
     env = parent.frame()
-    # env = as.environment("package:ARdetection.Pan")
+    env = as.environment("package:ARdetection.Pan")
     file = system.file("python/ar_iden.py", package="ARdetection.Pan")
     source_python(file, env)
 }
